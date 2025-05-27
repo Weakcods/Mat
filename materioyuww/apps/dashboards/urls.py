71fbc06c835d359d78b3a8b6ae94043b2a21f3fd
Joshua Bacay <bacayjhoshuajm@gmail.com>
2025-05-27 23:00:39 +0800

    add ui pages

--- materioyuww/apps/dashboards/urls.py
+++ materioyuww/apps/dashboards/urls.py
@@ -1,12 +1,35 @@
 from django.urls import path
 from .views import DashboardsView
 
-
-
 urlpatterns = [
+    path(
+        "analytics/",
+        DashboardsView.as_view(template_name="dashboard_analytics.html"),
+        name="dashboard-analytics",
+    ),
+    path(
+        "crm/",
+        DashboardsView.as_view(template_name="dashboard_crm.html"),
+        name="dashboard-crm",
+    ),
+    path(
+        "ecommerce/",
+        DashboardsView.as_view(template_name="dashboard_ecommerce.html"),
+        name="dashboard-ecommerce",
+    ),
+    path(
+        "logistics/",
+        DashboardsView.as_view(template_name="dashboard_logistics.html"),
+        name="dashboard-logistics",
+    ),
+    path(
+        "academy/",
+        DashboardsView.as_view(template_name="dashboard_academy.html"),
+        name="dashboard-academy",
+    ),
     path(
         "",
         DashboardsView.as_view(template_name="dashboard_analytics.html"),
         name="index",
-    )
+    ),
 ]
